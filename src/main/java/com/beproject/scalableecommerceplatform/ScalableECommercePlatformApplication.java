155f3af9129f110035cbe1fa33d45e620e03e596
darshancodes2612 <joshi.darshan1826@gmail.com>
2024-11-26 01:18:26 +0530

    created the folder structure for all the microservices*

---
+++ src/main/java/com/beproject/scalableecommerceplatform/ScalableECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.beproject.scalableecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableECommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalableECommercePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beproject/scalableecommerceplatform/ScalableECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beproject.scalableecommerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableECommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
